6017cbbb733dc3ec567afa6f5fe1fcb5e2105800
anshuldhargave <dhargaveanshul@gmail.com>
2023-11-25 18:29:36 +0000

    AD: Started wokring on the functionality.

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ console.log("Hello world");
 
 // alert("Hello");
 
+const myButton = document.getElementById('1-button')
+
+myButton.addEventListener('click', function(event) {
+  
+    console.log("Button 1 clicked");
+    // const myInputValue = myInput.value;
+    
+    // content.innerHTML = myInputValue
+  })
+  
+
+  
